8e9b60d3e46edc5ddca081759e486eef1c7cee6b
thihuli <thiagocandidof@hotmail.com>
2024-06-16 12:30:29 -0300

    chore: remove cod

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
   controllers: [UsersController],
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,49 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { hash } from "bcryptjs";
+import { Injectable } from '@nestjs/common';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const emailTaken = await this.usersRepo.findByEmail(createUserDto.email);
-
-    if (emailTaken) {
-      throw new ConflictException('Email already taken');
-    }
-
-    const hashedPassword = await hash(createUserDto.password, 10);
-
-    const user = await this.usersRepo.create({
-      data: {
-        ...createUserDto,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              // Income
-              { name: 'Salário', icon: 'salary', type: 'INCOME' },
-              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-              { name: 'Outro', icon: 'other', type: 'INCOME' },
-              // Expense
-              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
-              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
-              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
-            ]
-          }
-        }
-      },
-    })
-
-    return {user, message: 'User created successfully'};
+  getUserById(id: string) {
+    // return this.usersRepo.findById(id);
+    return { id, name: 'John Doe'}
   }
-
 }
