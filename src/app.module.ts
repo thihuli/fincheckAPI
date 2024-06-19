1c761ffa36a1440a79c606537a131d3fb891388c
thihuli <thiagocandidof@hotmail.com>
2024-06-18 22:31:18 -0300

    chore: configure dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { AuthGuard } from './modules/auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './modules/auth/auth.guard';
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
