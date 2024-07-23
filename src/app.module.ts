1ff5e767b6c926ef79a5de3613099f6b3fedc3e9
= <thiago.fernandes@instivo.com.br>
2024-07-22 22:21:37 -0300

    feat: create module categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
   controllers: [],
   providers: [
     {
